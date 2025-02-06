782072538fb40b82c285bcf4c69367451896a9da
kasparendon10 <carlosrendong1994@gmail.com>
2025-02-06 15:29:01 -0500

    app movil

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/style.css',  // Asegúrate de que el archivo esté en la misma carpeta
+          '/script.js',  // Lo mismo para el archivo JS
+          '/icono.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  });
+  
